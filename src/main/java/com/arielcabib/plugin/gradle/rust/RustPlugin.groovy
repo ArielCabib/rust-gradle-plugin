03a30deffc4d08fdea420bd74962b556d41b081e
Josselin Chevalay <josselin.chevalay@itiviti.com>
2021-02-18 16:12:44 +0100

    Update gradle disable leftShift method
    Change-Id: Iddda0007cfaedea6575a618e1e9b25c7fc52bd21

--- src/main/java/com/arielcabib/plugin/gradle/rust/RustPlugin.groovy
+++ src/main/java/com/arielcabib/plugin/gradle/rust/RustPlugin.groovy
@@ -2,6 +2,7 @@ package com.arielcabib.plugin.gradle.rust
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 
 /**
  * Created by ariel on 17/05/15.
@@ -9,26 +10,17 @@ import org.gradle.api.Project
 class RustPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.task('rustBuild') << {
-            println "Cargo building"
-            project.exec {
-                commandLine 'cargo', 'build'
-            }
+        project.tasks.create('rustBuild', Exec).configure {
+             commandLine 'cargo', 'build'
         }
 
-        project.task('rustRun') << {
-            println "Cargo running"
-            project.exec {
-                standardInput System.in
-                commandLine 'cargo', 'run'
-            }
+        project.tasks.create('rustRun', Exec).configure {
+            standardInput System.in
+            commandLine 'cargo', 'run'
         }
 
-        project.task('rustUpdate') << {
-            println "Cargo updating"
-            project.exec {
-                commandLine 'cargo', 'update'
-            }
+        project.tasks.create('rustUpdate', Exec).configure {
+             commandLine 'cargo', 'update'
         }
     }
 }
